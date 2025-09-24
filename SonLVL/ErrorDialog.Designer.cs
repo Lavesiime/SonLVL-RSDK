f0776bbf4455c0500914cbf6aab9e0d5ae383fa9
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2025-09-24 00:56:59 -0500

    Error Dialog: Rename "Report" button to "Details"

--- SonLVL/ErrorDialog.Designer.cs
+++ SonLVL/ErrorDialog.Designer.cs
@@ -65,7 +65,7 @@ namespace SonicRetro.SonLVL.GUI
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 2;
-			this.button1.Text = "Report...";
+			this.button1.Text = "Details...";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
