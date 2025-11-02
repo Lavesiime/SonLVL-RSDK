7c87cd7fc0943e0efa4b025fdf5c89266f137a38
Lave sIime <70488825+Lavesiime@users.noreply.github.com>
2025-11-02 02:33:12 -0600

    SonLVL - Don't enable "Continue" button for errors where the editor would be frozen after pressing the Continue button anyways

--- SonLVL/MainForm.cs
+++ SonLVL/MainForm.cs
@@ -87,7 +87,7 @@ namespace SonicRetro.SonLVL.GUI
 		{
 			Log(e.Exception.ToString().Split(new string[] { Environment.NewLine }, StringSplitOptions.None));
 			File.WriteAllLines("SonLVL.log", LogFile.ToArray());
-			using (ErrorDialog ed = new ErrorDialog("Unhandled Exception " + e.Exception.GetType().Name + "\nLog file has been saved.\n\nDo you want to try to continue running?", true))
+			using (ErrorDialog ed = new ErrorDialog("Unhandled Exception " + e.Exception.GetType().Name + "\nLog file has been saved.\n\nDo you want to try to continue running?", Enabled))
 			{
 				if (ed.ShowDialog(this) == DialogResult.Cancel)
 					Close();
