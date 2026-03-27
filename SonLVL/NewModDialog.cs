39012284ff23792c2a24ce819ab113ec51e1699b
Lave siime <70488825+Lavesiime@users.noreply.github.com>
2026-03-26 22:20:30 -0500

    Small mod folder creation fix

--- SonLVL/NewModDialog.cs
+++ SonLVL/NewModDialog.cs
@@ -33,7 +33,7 @@ namespace SonicRetro.SonLVL.GUI
 
 		private void buttonOK_Click(object sender, EventArgs e)
 		{
-			string moddir = Path.Combine(LevelData.EXEFolder, "mods", ValidateFilename(textModName.Text));
+			string moddir = Path.Combine(LevelData.EXEFolder, "mods", ValidateFilename(textModName.Text.Trim()));
 
 			if (textModName.Text.Length <= 0)
 			{
